57481cc693fa3950996052e9f73b1a0b678310dc
Gaetan Nadon <memsize@videotron.ca>
2009-07-17 13:35:22 -0400

    build.sh: driver/xf86-video-nouveau does not exist
    This patch removes the non-existing component from the build list.
    https://bugs.freedesktop.org/show_bug.cgi?id=22824
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- build.sh
+++ build.sh
@@ -587,7 +587,6 @@ build_driver_video() {
     build driver xf86-video-mga
     build driver xf86-video-neomagic
     build driver xf86-video-newport
-    build driver xf86-video-nouveau
     build driver xf86-video-nv
     build driver xf86-video-radeonhd
     build driver xf86-video-rendition
