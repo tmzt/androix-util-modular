983118a28246f0bd2e378d6f97b29b28a0325c49
Alan Coopersmith <alan.coopersmith@sun.com>
2009-11-06 19:54:40 -0800

    Update haschanged.sh to use git instead of cvs
    Signed-off-by: Alan Coopersmith <alan.coopersmith@sun.com>

--- haschanged.sh
+++ haschanged.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-if test x$1 = x; then
-    echo Usage $0 tag
-    exit 1
+# Usage: haschanged.sh [-l] [tag]
+#
+# Show changes in git for all modules since either tag specified on command
+# line or the most recent tag on main branch (as found by git describe)
+#
+# If -l is given, shows git log since tag
+
+SHOW_LOG=0
+
+if test "x$1" = "x-l" ; then
+    SHOW_LOG=1
+    shift
 fi
 
 TAG=$1
@@ -14,13 +23,30 @@ haschanged() {
     cd $1
     for i in `ls -1`; do
 	if test -e $i/configure.ac; then
-	    CHANGED=no
-	    cvs diff -uN -r $TAG -r HEAD $i > /dev/null 2>&1 || CHANGED=yes
-	    if test $CHANGED = yes; then
-		echo $1/$i has changed since $TAG
+	    cd $i
+	    if test "x${TAG}" != "x" ; then
+		LAST_TAG=`git tag -l $TAG`
 	    else
-		echo $1/$i has not changed since $TAG
+		LAST_TAG=`git describe --abbrev=0`
 	    fi
+	    if test "x${LAST_TAG}" = "x" ; then
+		echo $1/${i}: tag ${TAG} not found
+	    else
+		HEAD_DESC=`git describe`
+
+		if test "${HEAD_DESC}" != "${LAST_TAG}" ; then
+		    echo $1/$i has changed since $LAST_TAG
+		    if test "${SHOW_LOG}" = 1 ; then
+			echo ''
+			git log "${LAST_TAG}"..
+			echo '============================================'
+			echo ''
+		    fi
+		else
+		    echo $1/$i has not changed since $LAST_TAG
+		fi
+	    fi
+	    cd ..
 	fi
     done
     cd ..
